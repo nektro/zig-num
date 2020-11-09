499d5861ca7fb5488cfece3d4204162ed95d36f6
Meghan <hello@nektro.net>
2020-11-09 13:52:46 -0800

    add pub to fields

--- src/main.zig
+++ src/main.zig
@@ -1,17 +1,17 @@
 const std = @import("std");
 const testing = std.testing;
 
-export const zero: i32 = 0;
-export const one: i32 = 1;
-export const two: i32 = 2;
-export const three: i32 = 3;
-export const four: i32 = 4;
-export const five: i32 = 5;
-export const six: i32 = 6;
-export const seven: i32 = 7;
-export const eight: i32 = 8;
-export const nine: i32 = 9;
-export const ten: i32 = 10;
+pub export const zero: i32 = 0;
+pub export const one: i32 = 1;
+pub export const two: i32 = 2;
+pub export const three: i32 = 3;
+pub export const four: i32 = 4;
+pub export const five: i32 = 5;
+pub export const six: i32 = 6;
+pub export const seven: i32 = 7;
+pub export const eight: i32 = 8;
+pub export const nine: i32 = 9;
+pub export const ten: i32 = 10;
 
 test "basic num functionality" {
     testing.expect(zero == 0);
